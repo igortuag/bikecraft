1859de8306764c7e7d9316e26f7c45b44c992079
Igor Tuag <igortuag@gmail.com>
2020-12-13 08:04:33 -0300

    [5-10] - custom fields introducao include

--- wordpress-theme/inc/introducao.php
+++ wordpress-theme/inc/introducao.php
@@ -1,6 +1,13 @@
-<section class="introducao-interna interna_contato">
+<style type="text/css">
+.introducao-interna {
+  background: url("<?php the_field('background_interno'); ?>") no-repeat center;
+  background-size: cover;
+} 
+</style>
+
+<section class="introducao-interna">
   <div class="container">
-    <h1 data-anime="400" class="fadeInDown">Contato</h1>
-    <p data-anime="800" class="fadeInDown">Tire suas duvidas com a gente</p>
+    <h1 data-anime="400" class="fadeInDown"><?php the_title(); ?></h1>
+    <p data-anime="800" class="fadeInDown"><?php the_field('subtitulo'); ?></p>
   </div>
 </section>
