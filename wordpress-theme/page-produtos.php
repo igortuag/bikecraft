50d35fb85abde72c7d4b7dff7a44c8749e328a78
Igor Tuag <igortuag@gmail.com>
2020-12-15 07:20:03 -0300

    [5-16] - create include orcamento

--- wordpress-theme/page-produtos.php
+++ wordpress-theme/page-produtos.php
@@ -82,44 +82,7 @@ get_header();
       </div>
     </section>
 
-    <section class="orcamento">
-      <div class="container">
-        <h2 class="subtitulo">Orçamento</h2>
-        <form id="form_orcamento" class="form grid-8 formphp" method="POST" action="./enviar.php">
-          <label for="nome">Nome</label>
-          <input type="text" id="nome" name="nome" required />
-          <label for="email">E-mail</label>
-          <input type="email" id="email" name="email" required />
-          <label for="telefone">Telefone</label>
-          <input type="text" id="telefone" name="telefone" />
-          <label for="mensagem">Mensagem</label>
-
-          <label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
-          <input type="text" class="nao-aparece" name="leaveblank" />
-          <label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
-          <input type="text" class="nao-aparece" name="dontchange" value="http://" />
-
-          <textarea id="mensagem" name="mensagem" required></textarea>
-          <button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
-        </form>
-
-        <div class="orcamento_dados grid-8">
-          <h3>Dados</h3>
-          <span>+55 27 99999 9999</span>
-          <span>orcamento@bikcraft.com</span>
-
-          <h3>Monte a sua Bikcraft</h3>
-          <p>Escolha as especificações</p>
-          <ul>
-            <li>- Cores</li>
-            <li>- Estilo</li>
-            <li>- Medidas</li>
-            <li>- Acessórios</li>
-            <li>- E Outros</li>
-          </ul>
-        </div>
-      </div>
-    </section>
+    <?php include(TEMPLATEPATH . "/inc/orcamento.php"); ?>
 
     <?php endwhile; else: endif; ?>
     
--- wordpress-theme/single-produtos.php
+++ wordpress-theme/single-produtos.php
@@ -1,5 +1,3 @@
-<?php $contato = get_page_by_title('contato'); ?>
-
 <?php
 // Template Name: Single Produtos
 get_header();
@@ -23,45 +21,8 @@ get_header();
       </div>
     </section>
 
-    <section class="orcamento">
-      <div class="container">
-        <h2 class="subtitulo">Orçamento</h2>
-        <form id="form_orcamento" class="form grid-8 formphp" method="POST" action="./enviar.php">
-          <label for="nome">Nome</label>
-          <input type="text" id="nome" name="nome" required />
-          <label for="email">E-mail</label>
-          <input type="email" id="email" name="email" required />
-          <label for="telefone">Telefone</label>
-          <input type="text" id="telefone" name="telefone" />
-          <label for="mensagem">Mensagem</label>
-
-          <label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
-          <input type="text" class="nao-aparece" name="leaveblank" />
-          <label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
-          <input type="text" class="nao-aparece" name="dontchange" value="http://" />
-
-          <textarea id="mensagem" name="mensagem" required></textarea>
-          <button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
-        </form>
-
-        <div class="orcamento_dados grid-8">
-          <h3>Dados</h3>
-          <span><?php the_field('telefone', $contato); ?></span>
-          <span><?php the_field('email', $contato); ?></span>
-
-          <h3>Monte a sua Bikcraft</h3>
-          <p>Escolha as especificações</p>
-          <ul>
-            <li>- Cores</li>
-            <li>- Estilo</li>
-            <li>- Medidas</li>
-            <li>- Acessórios</li>
-            <li>- E Outros</li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
+    <?php include(TEMPLATEPATH . "/inc/orcamento.php"); ?>
+   
     <?php endwhile; else: endif; ?>
     
 <?php get_footer(); ?>
