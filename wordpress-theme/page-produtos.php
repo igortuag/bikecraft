dbe00411de4f1f7612911d028764199cb94bc19f
Igor Tuag <igortuag@gmail.com>
2020-12-15 07:35:58 -0300

    [5-17] config links

--- wordpress-theme/page-produtos.php
+++ wordpress-theme/page-produtos.php
@@ -16,13 +16,15 @@ get_header();
 
     <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
       <section class="container produtos_item fadeInDown" data-anime="1200">
-        <div class="grid-11">
-          <img src="<?php the_field('foto_produto1'); ?>" alt="Bikcraft <?php the_title(); ?>" />
-          <h2><?php the_title(); ?></h2>
-        </div>
-        <div class="grid-5 produto_icone">
-          <img src="<?php the_field('icone_produto'); ?>" alt=Bikcraft <?php the_title(); ?>" />
-        </div>
+        <a href="<?php the_permalink(); ?>">
+          <div class="grid-11">
+            <img src="<?php the_field('foto_produto1'); ?>" alt="Bikcraft <?php the_title(); ?>" />
+            <h2><?php the_title(); ?></h2>
+          </div>
+          <div class="grid-5 produto_icone">
+            <img src="<?php the_field('icone_produto'); ?>" alt=Bikcraft <?php the_title(); ?>" />
+          </div>
+        </a>
       </section>
     <?php endwhile; else: endif; ?>
 
