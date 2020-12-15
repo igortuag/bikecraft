e467ec0e0c66b606bc83d95bfeeda394a2595bb1
Igor Tuag <igortuag@gmail.com>
2020-12-15 07:33:08 -0300

    [5-17] - add product pages on loop

--- wordpress-theme/page-produtos.php
+++ wordpress-theme/page-produtos.php
@@ -15,83 +15,16 @@ get_header();
     ?>
 
     <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-      <h1><?php the_title(); ?></h1>
+      <section class="container produtos_item fadeInDown" data-anime="1200">
+        <div class="grid-11">
+          <img src="<?php the_field('foto_produto1'); ?>" alt="Bikcraft <?php the_title(); ?>" />
+          <h2><?php the_title(); ?></h2>
+        </div>
+        <div class="grid-5 produto_icone">
+          <img src="<?php the_field('icone_produto'); ?>" alt=Bikcraft <?php the_title(); ?>" />
+        </div>
+      </section>
     <?php endwhile; else: endif; ?>
-    
-    <section class="container produtos_item fadeInDown" data-anime="1200">
-      <div class="grid-11">
-        <img src="./img/produtos/bikcraft-passeio-1.jpg" alt="Bikcraft Passeio" />
-        <h2>Passeio</h2>
-      </div>
-      <div class="grid-5 produto_icone">
-        <img src="./img/produtos/passeio.svg" alt="Bikcraft Passeio" />
-      </div>
-      <div class="grid-8">
-        <img src="./img/produtos/bikcraft-passeio-2.jpg" alt="Bikcraft Passeio" />
-      </div>
-      <div class="grid-8 produto_info">
-        <p>
-          No mundo atual, a contínua expansão de nossa atividade cumpre um papel essencial na formulação da gestão
-          inovadora da qual fazemos parte da qual fazemos parte.
-        </p>
-        <ul>
-          <li>Conforto</li>
-          <li>Velocidade</li>
-          <li>Design</li>
-          <li>Versatilidade</li>
-        </ul>
-      </div>
-    </section>
-
-    <section class="container produtos_item">
-      <div class="grid-11">
-        <img src="./img/produtos/bikcraft-esporte-1.jpg" alt="Bikcraft esporte" />
-        <h2>Esporte</h2>
-      </div>
-      <div class="grid-5 produto_icone">
-        <img src="./img/produtos/esporte.svg" alt="Bikcraft Esporte" />
-      </div>
-      <div class="grid-8">
-        <img src="./img/produtos/bikcraft-esporte-2.jpg" alt="Bikcraft Esporte" />
-      </div>
-      <div class="grid-8 produto_info">
-        <p>
-          No mundo atual, a contínua expansão de nossa atividade cumpre um papel essencial na formulação da gestão
-          inovadora da qual fazemos parte da qual fazemos parte.
-        </p>
-        <ul>
-          <li>Conforto</li>
-          <li>Velocidade</li>
-          <li>Design</li>
-          <li>Versatilidade</li>
-        </ul>
-      </div>
-    </section>
-
-    <section class="container produtos_item">
-      <div class="grid-11">
-        <img src="./img/produtos/bikcraft-retro-1.jpg" alt="Bikcraft Retro" />
-        <h2>Retro</h2>
-      </div>
-      <div class="grid-5 produto_icone">
-        <img src="./img/produtos/retro.svg" alt="Bikcraft Retro" />
-      </div>
-      <div class="grid-8">
-        <img src="./img/produtos/bikcraft-retro-2.jpg" alt="Bikcraft retro" />
-      </div>
-      <div class="grid-8 produto_info">
-        <p>
-          No mundo atual, a contínua expansão de nossa atividade cumpre um papel essencial na formulação da gestão
-          inovadora da qual fazemos parte da qual fazemos parte.
-        </p>
-        <ul>
-          <li>Conforto</li>
-          <li>Velocidade</li>
-          <li>Design</li>
-          <li>Versatilidade</li>
-        </ul>
-      </div>
-    </section>
 
     <?php include(TEMPLATEPATH . "/inc/orcamento.php"); ?>
 
